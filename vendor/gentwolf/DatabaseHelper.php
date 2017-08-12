0885ee70e47b76f82ab0086d7ffa4142659654d8
jerry <lzshenyun@163.com>
2017-08-12 20:50:12 +0800

    update

--- vendor/gentwolf/DatabaseHelper.php
+++ vendor/gentwolf/DatabaseHelper.php
@@ -18,15 +18,19 @@ class DatabaseHelper {
 	public static function fetchAll($table, $fields, $where, $ext = null, $dbConfig = 'default') {
 		$params = [];
 		$data = [];
-		foreach ($where as $key => $value) {
+		if ($where) foreach ($where as $key => $value) {
 			$tmp = explode(' ', $key);
 			if (count($tmp) == 1) $tmp[1] = '=';
 
-			$params[] = $tmp[0] .' '. $tmp[1] .' :'. $tmp[0];
-			$data[':'. $tmp[0]] = $value;
+			$param = $tmp[0];
+			if (strpos($param, '.') > 0) $param = explode('.', $param)[1];
+
+			$params[] = $tmp[0] .' '. $tmp[1] .' :'. $param;
+			$data[':'. $param] = $value;
 		}
 
-		$sql = 'SELECT '. $fields .' FROM '. $table .' WHERE '. implode(' AND ', $params);
+		$sql = 'SELECT '. $fields .' FROM '. $table;
+		if (count($params) > 0) $sql .= ' WHERE '. implode(' AND ', $params);
 		if ($ext != null) $sql .= ' '. $ext;
 		$cmd = Database::driver($dbConfig)->createCommand($sql);
 		return $cmd->execute($data);
@@ -74,11 +78,17 @@ class DatabaseHelper {
 	 */
 	public static function update($table, $items, $where, $dbConfig = 'default') {
 		$data = [];
+		$updateData = '';
 
-		$params1 = [];
-		foreach ($items as $key => $value) {
-			$params1[] = $key .'=:'. $key;
-			$data[':'. $key] = $value;
+		if (is_array($items)) {
+			$params1 = [];
+			foreach ($items as $key => $value) {
+				$params1[] = $key .'=:'. $key;
+				$data[':'. $key] = $value;
+			}
+			$updateData = implode(',', $params1);
+		} else {
+			$updateData = $items;
 		}
 
 		$params2 = [];
@@ -90,7 +100,7 @@ class DatabaseHelper {
 			$data[':'. $tmp[0]] = $value;
 		}
 
-		$sql = 'UPDATE '. $table .' SET '. implode(',', $params1) .' WHERE '. implode(' AND ', $params2);
+		$sql = 'UPDATE '. $table .' SET '. $updateData .' WHERE '. implode(' AND ', $params2);
 		$cmd = Database::driver($dbConfig)->createCommand($sql);
 		return $cmd->execute($data);
 	}
